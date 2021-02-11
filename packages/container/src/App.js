1e81eb78cd12c1ed5727b9ab66484b2007ff8222
Jeongah Yi <jeongahyi@Jeongahui-MacBookPro.local>
2021-02-11 18:33:00 +0900

    Change container typo to test dev branch

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -8,7 +8,7 @@ import MarketingApp from './components/MarketingApp';
 export default () => {
     return (
     <div>
-        <h1>GOOD LUCK</h1>
+        <h1>landing page</h1>
         <hr />
         <MarketingApp />
     </div>);
