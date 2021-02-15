9d3351140e4b47444f8733e57ba86f6e15eea473
Jeongah Yi <jeongahyi@Jeongahui-MacBookPro.local>
2021-02-15 15:47:34 +0900

    Add communication between container and marketing

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -4,9 +4,25 @@ import { createMemoryHistory } from 'history';
 import App from './App';
 
 // Mount function to start up the app
-const mount = (el) => {
+const mount = (el, { onNavigate }) => {
     const history = createMemoryHistory();
+
+    if (onNavigate) {
+        history.listen(onNavigate);
+    }
+
     ReactDOM.render(<App history={history} />,el);
+
+    return {
+        onParentNavigate({ pathname: nextPathname }) {
+            const { pathname } = history.location;
+
+            if (pathname !== nextPathname) {
+                history.push(nextPathname);
+            }
+            console.log(location);
+        }
+    };
 };
 
 // If we are in development and in isolation,
@@ -15,7 +31,7 @@ if (process.env.NODE_ENV === 'development') {
     const devRoot = document.querySelector('#_marketing-dev-root');
 
     if (devRoot) {
-        mount(devRoot);
+        mount(devRoot, {});
     }
 }
 
